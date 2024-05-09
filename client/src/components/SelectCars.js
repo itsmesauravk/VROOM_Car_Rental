a16f280c3fafc610bab6b37397f97a96bf12799a
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2024-05-09 21:38:08 +0545

    Distributor Car Selection Completed

--- client/src/components/SelectCars.js
+++ client/src/components/SelectCars.js
@@ -1,39 +1,71 @@
-import React, { useContext, useEffect } from 'react'
-import "../css/selectcars.css"
-import { SelectCarContext } from './SelectCarContext'
+import React, { useContext, useEffect, useState } from 'react';
+import "../css/selectcars.css";
+import { SelectCarContext } from './SelectCarContext';
+import { IoMdClose } from "react-icons/io";
 
 const SelectCars = () => {
-    const {handleSelectCar}=useContext(SelectCarContext)
+    const { handleSelectCar } = useContext(SelectCarContext);
+    const [carsList, setCarsList] = useState([]); // Initialize with empty array
 
     const token = localStorage.getItem('token');
-  
+
     const getAllDistCars = async () => {
-      try {
-        const response = await fetch('http://localhost:4000/get-dist-cars', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}` 
-          }
-        });
-        const result = await response.json();
-        console.log(result);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    
+        try {
+            const response = await fetch('http://localhost:4000/get-dist-cars', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            const result = await response.json();
+            setCarsList(result.rentalClients);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+useEffect(() => {
+    getAllDistCars();
+}, []);
 
-    useEffect(() => {
-      getAllDistCars();
-    }, [])
-
-  return (
+return (
     <div className='selectcarsdiv'>
-      This is select cars.
-      <button onClick={handleSelectCar}>Close</button>
+        <div className='available-div'>
+            <h1>Available Cars:</h1>
+            <button onClick={handleSelectCar} className='select-car-button'>
+                <IoMdClose className='cars-close'/>
+            </button>
+        </div>
+        <div className='select-cars-list'>
+            {carsList.length === 0 ? (
+                <div>No cars available</div>
+            ) : (
+              <>
+                <div className='list-details'>
+                  <h4>S.N.</h4>
+                  <h4>Car Image</h4>
+                  <h4>Car Type</h4>
+                  <h4>Vehicle No.</h4>
+                  <h4>Driver Name</h4>
+                </div>
+                {carsList.map((car,index)=>{
+                  return(
+                    <div className='actual-details' key={car.carNumber}>
+                      <p>{index+1}</p>
+                      <img src= {`http://localhost:4000/${car.carPhoto}`} alt='car-photo' className="car-detail-photo"></img>
+                      <p>{car.carType}</p>
+                      <p>{car.carNumber}</p>
+                      <p>{car.driverName}</p>
+                    </div>
+                  )
+                })}
+                
+              </>
+            )}
+        </div>
     </div>
-  )
-}
+);
 
-export default SelectCars
+};
+
+export default SelectCars;
